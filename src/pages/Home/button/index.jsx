79242a5505a9e58913ea726f32340ea1b068937c
fuyiwei <wwwfuyiwei@qq.com>
2019-05-28 11:03:08 +0800

    style(修改界面样式): 按钮高度修改
    100vh修改为50vh

--- src/pages/Home/button/index.jsx
+++ src/pages/Home/button/index.jsx
@@ -5,7 +5,7 @@ import { TitleContext } from '../context'
 
 const StyledText = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 50vh;
   display: flex;
   justify-content: center;
   align-items: center;
